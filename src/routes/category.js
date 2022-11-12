852541efa41d9313a083e7195ab4f09a87e649c5
Truong Tan Dat <truongtandat1409@gmail.com>
2022-11-12 17:46:24 +0700

    fix roleADmin cate

--- src/routes/category.js
+++ src/routes/category.js
@@ -5,7 +5,7 @@ const router = require("express").Router();
 
 router.get("/category", role("Admin"), categoryController.getAllCategories);
 router.patch("/category/update", categoryController.updateCategory);
-router.post("/category", categoryController.createNewCategory);
+router.post("/category", role("Admin"), categoryController.createNewCategory);
 router.delete(
   "/category/:id",
   role("Admin"),
